02ec37e06e661b0eb1f670885ed66e084fac6980
Petr Doroshev <doroshevpetr@gmail.com>
2026-01-02 19:10:28 +0300

    sorting and sum implemented

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <sstream>
 #include <iomanip>
 #include <vector>
+#include <algorithm>
+#include <numeric>
 #include <memory>
 #include "Point3D.h"
 #include "curves/Circle.h"
@@ -17,10 +19,11 @@ int main(int argc, char* argv[]) {
         return 0;
     }
 
-    std::string path = "../examples/curves_to_check.txt";
+    std::string path = "../examples/additional_curves_1.txt";
     std::ifstream file(path);
 
     if (!file.is_open()) {
+
         std::cerr << "Unable to open file: " << path << std::endl;
         return 1;
     }
@@ -35,6 +38,7 @@ int main(int argc, char* argv[]) {
         line_number = std::stoi(line);
     } 
     catch (const std::invalid_argument& e) {
+
         std::cerr << "Unable to read number of lines: " << e.what() << std::endl; 
         return 1;
     }
@@ -61,42 +65,97 @@ int main(int argc, char* argv[]) {
             return 1;
         }
 
-        if (radius < 0) {
-            std::cout << radius << "\n";
-            std::cerr << "Radius can't be negative" << "\n";
-            return 1;
+        if (radius <= 0) {
+            std::cerr << "Radius can't be negative: id: " << id << "\n";
+            continue;
         }
 
         if (curve_type == 'C') {
             
-            curves.emplace_back(std::make_shared<Circle>(Circle(id, curve_name, center, radius)));
+            curves.emplace_back(std::make_shared<Circle>(id, curve_name, center, radius));
 
         }
         else if (curve_type == 'E') {
 
             double minor_radius = 0.0;
-            ss >> minor_radius;
+            
+            if (!(ss >> minor_radius)) {
 
-            curves.emplace_back(std::make_shared<Ellipse>(Ellipse(id, curve_name, center, radius, minor_radius)));
+                std::cerr << "Unable to parse line: " << "\"" << line << "\"" << "\n";
+                return 1;
+            }
+
+            if (minor_radius <= 0) {
+                
+                std::cerr << "Radius can't be negative: id: " << id << "\n";
+                continue;
+            }
+
+            curves.emplace_back(std::make_shared<Ellipse>(id, curve_name, center, radius, minor_radius));
         }
         else if (curve_type == 'H') {
 
             double step = 0.0;
-            ss >> step;
 
-            curves.emplace_back(std::make_shared<Helix>(Helix(id, curve_name, center, radius, step)));
+            if (!(ss >> step)) {
+                
+                std::cerr << "Unable to parse line: " << "\"" << line << "\"" << "\n";
+                return 1;
+            }
+
+            curves.emplace_back(std::make_shared<Helix>(id, curve_name, center, radius, step));
 
         }
     }
 
+    std::vector<const Circle*> circles; 
+
+    std::cout << std::left
+              << std::setw(15)  << "ID"
+              << std::setw(20) << "Name"
+              << std::setw(30) << "Point (t=pi/4)"
+              << std::setw(30) << "Derivative (t=pi/4)"
+              << '\n';
+
+    std::cout << std::string(88, '-') << '\n';
+
     for (auto& c: curves) {
         
-        std::cout << "id: " << c->getId() << "  name: " << c->getName() << "  " << c->point(M_PI / 4).toString()  << "\n";
-        
+        std::cout << std::left 
+                  << std::setw(15) << c->getId()
+                  << std::setw(20) << c->getName()
+                  << std::setw(30) << c->point(M_PI / 4).toString()
+                  << std::setw(30) << c->derivative(M_PI / 4).toString()
+                  << '\n';
 
+    
+        if (auto circle = dynamic_cast<const Circle*>(c.get())) {
+            circles.push_back(circle);
+        }
     }
 
 
+    //Sort circles by their radii in ascending order
+    std::sort(circles.begin(), circles.end(), [](const Circle* c1, const Circle* c2) {
+
+        return c1->getRadius() < c2->getRadius();
+
+    });
+
+    //Сalculate the sum of the radii
+    auto radius_sum = std::accumulate(circles.begin(), circles.end(), 0.0, [](double sum, const Circle* c) {
+        return sum + c->getRadius();
+    });
+    
+
+    std::cout << "\nCircles: \n";
+    for (auto& c: circles) {
+
+        std::cout << "id: " << c->getId() << ", " << "radius: " <<  c->getRadius() << "\n";
+    }
+
+    std::cout << "\nSum of radii: " << radius_sum << "\n";
     
     return 0;
 }
+
